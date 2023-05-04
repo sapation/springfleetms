0bfbe4c84aa0506162545de8a0da7473ff0c98c8
iddrisu <sumaila129@gmail.com>
2023-05-04 14:26:46 +0000

    first commit

---
+++ src/main/java/com/webtech/fleetmsv2/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.webtech.fleetmsv2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+}
